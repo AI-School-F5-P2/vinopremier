d40152b1b83f96abdd81ca617920ab43cfd05408
AlexVennegas <erick.alexis.venegas@gmail.com>
2024-02-09 13:19:01 +0100

    Add exception handling for API endpoint

--- API/main.py
+++ API/main.py
@@ -4,7 +4,7 @@ import pickle
 from pydantic import BaseModel
 from typing import List
 from sklearn.preprocessing import LabelEncoder, MinMaxScaler
-
+from fastapi.responses import JSONResponse
 # INSTANCIA DE FASTAPI
 app = FastAPI()
 
@@ -32,49 +32,52 @@ def read_root():
 @app.post("/prueba")
 def encontrar_vinos_similares(data_vino: data_vino):
     # PASAR A UNA DICT
-    data_vino = data_vino.dict()
-    categoricas = ['uvas', 'añada', 'DO', 'tipo_crianza', 'meses_barrica', 'tipo_vino', 'bodega']
-    numericas = ['final_price']
+    try:
+        data_vino = data_vino.dict()
+        categoricas = ['uvas', 'añada', 'DO', 'tipo_crianza', 'meses_barrica', 'tipo_vino', 'bodega']
+        numericas = ['final_price']
+        
+        # Cargar el modelo y objetos necesarios desde el archivo pickle
+        with open('./Model/modelo.pickle', 'rb') as f:
+            knn_loaded, label_encoders_loaded, scaler_loaded = pickle.load(f)
+
+        def obtener_numero_codificado(columna, valor_original):
+            # Aplicar la transformación para obtener el número codificado
+            numero_codificado = label_encoders_loaded[columna].transform([valor_original])[0]
+            return numero_codificado
+        
+        # Generar un dataframe con todos los atributos
+        vino_agotado = pd.DataFrame(data_vino)
+        # Generar un dataframe con todos los atributos
+        #vino_agotado = pd.DataFrame(data_vino)
+
+        # Extraer solo las características categóricas
+        vino_agotado_categoricas = vino_agotado[categoricas]
+
+        # Aplicar la función obtener_numero_codificado a las características categóricas de data_vino
+        # para recuperar los números con los que se codificó anteriormente al hacer el entrenamiento del modelo
+        for columna in categoricas:
+            vino_agotado_categoricas.loc[:, columna] = obtener_numero_codificado(columna, vino_agotado_categoricas[columna].iloc[0])
+
+        # Agregar las variables numéricas
+        vino_agotado_categoricas[numericas] = vino_agotado[numericas]
+
+        # Escalar todos los atributos
+        vino_agotado_scaled = scaler_loaded.transform(vino_agotado_categoricas)
+
+        # Realizar predicciones
+        vino_agotado_codificado = vino_agotado_scaled.reshape(1, -1) # Reshape para satisfacer las dimensiones esperadas
+        distancias, indices = knn_loaded.kneighbors(vino_agotado_codificado)
     
-    # Cargar el modelo y objetos necesarios desde el archivo pickle
-    with open('./Model/modelo.pickle', 'rb') as f:
-        knn_loaded, label_encoders_loaded, scaler_loaded = pickle.load(f)
+        
 
-    def obtener_numero_codificado(columna, valor_original):
-        # Aplicar la transformación para obtener el número codificado
-        numero_codificado = label_encoders_loaded[columna].transform([valor_original])[0]
-        return numero_codificado
-    
-    # Generar un dataframe con todos los atributos
-    vino_agotado = pd.DataFrame(data_vino)
-    # Generar un dataframe con todos los atributos
-    #vino_agotado = pd.DataFrame(data_vino)
+        vino_similar_indices = indices[0]
+        vino_similar = df.iloc[vino_similar_indices]
 
-    # Extraer solo las características categóricas
-    vino_agotado_categoricas = vino_agotado[categoricas]
-
-    # Aplicar la función obtener_numero_codificado a las características categóricas de data_vino
-    # para recuperar los números con los que se codificó anteriormente al hacer el entrenamiento del modelo
-    for columna in categoricas:
-        vino_agotado_categoricas.loc[:, columna] = obtener_numero_codificado(columna, vino_agotado_categoricas[columna].iloc[0])
-
-    # Agregar las variables numéricas
-    vino_agotado_categoricas[numericas] = vino_agotado[numericas]
-
-    # Escalar todos los atributos
-    vino_agotado_scaled = scaler_loaded.transform(vino_agotado_categoricas)
-
-    # Realizar predicciones
-    vino_agotado_codificado = vino_agotado_scaled.reshape(1, -1) # Reshape para satisfacer las dimensiones esperadas
-    distancias, indices = knn_loaded.kneighbors(vino_agotado_codificado)
-   
-    
-
-    vino_similar_indices = indices[0]
-    vino_similar = df.iloc[vino_similar_indices]
-
-    # Descartar el mismo vino que se pasa a la función
-    vino_similar = vino_similar[vino_similar['SKU'] != data_vino['SKU'][0]]
-    
-    return vino_similar.to_dict(orient='records')
- 
+        # Descartar el mismo vino que se pasa a la función
+        vino_similar = vino_similar[vino_similar['SKU'] != data_vino['SKU'][0]]
+        
+        return vino_similar.to_dict(orient='records')
+    except Exception as e:
+        # return con tipo de error 400 en mi return y un mensaje de error con la e
+        return JSONResponse(content={"message": f"Hubo un problema al hacer la solicitud, Error: {e}"}, status_code=400)
