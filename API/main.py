29c507e543c89c818e537a044db04d4ed811a014
maikol <mgarrido2503@gmail.com>
2024-02-19 23:24:07 +0100

    add: model embeding

--- API/main.py
+++ API/main.py
@@ -13,7 +13,6 @@ from aplicar_relevancia import PuntuadorDeVinos
 from fastapi.middleware.cors import CORSMiddleware
 from buscar_vinos_similares_con_m_embeding import Predictor
 
-
 # INSTANCIA DE FASTAPI
 app = FastAPI()
 encontrar_vino = Encontrar_vinos_similares()
@@ -30,10 +29,8 @@ app.add_middleware(
     allow_headers=["*"],  # Encabezados permitidos
 )
 
-
 df = pd.read_csv('./Model/vinos_filtrados.csv')
 df_bodega = pd.read_csv('./Model/suppliers.csv')
-
 
 class sku(BaseModel):
     SKU: str
@@ -43,58 +40,52 @@ class sku(BaseModel):
 def read_root():
     return {"Hello": "World"}
 
-
+#MODELO KNN
 @app.post("/wineRecommendation")
 def encontrar_vinos_similares(sku: sku):
-    try:
-        
+    try:  
         sku = sku.SKU
         vinos_similares_list = encontrar_vino.encontrar_vinos_similares(sku, df)
         vinos_con_precio_mas_similar =  filtar.filtrar_por_precio(sku, vinos_similares_list, df)
         vinos_con_plan_marketin = plan_marketin.filtrar_vinos_con_plan_marketing(vinos_con_precio_mas_similar, df_bodega, df)
-        
         # unifico los vinos con plan de marketing y precio similares en un solo diccionario
         # para luego pasárselo a la función que va a puntuar con su nivel de relevancia
         vinos_precio_y_marketing = {'precio': vinos_con_precio_mas_similar, 'marketin': vinos_con_plan_marketin}
-        
         # aplico el nivel de relevancia 
         vinos_con_relevancia = puntuadorDeVinos.asignar_puntos_de_relevancia(vinos_precio_y_marketing, vinos_similares_list)
-        
         # elimino la descripción de los vinos
         for vino in vinos_con_relevancia:
             if 'description' in vino:
                 del vino['description']
-        a = type(vinos_similares_list)
         return  vinos_similares_list
-    
     except KeyError:
         raise HTTPException(status_code=404, detail="SKU no encontrado")
-    
     except ValueError as ve:
         raise HTTPException(status_code=400, detail=str(ve))
     
 
 
 
-
-
+# MODELO EMBEDING
 @app.post("/obtener_vinos_similares_con_m_embeding/")
 def obtener_vinos_similares_con_m_embeding(sku: sku):
-    sku = sku.SKU
-    vinos_similares_list = encontrar_vino_similare_con_m_embeding.predecir_vinos_similares_con_m_embeding(sku)
-    vinos_con_precio_mas_similar =  filtar.filtrar_por_precio(sku, vinos_similares_list, df)
-    vinos_con_plan_marketin = plan_marketin.filtrar_vinos_con_plan_marketing(vinos_con_precio_mas_similar, df_bodega, df)
+    try:
+        sku = sku.SKU
+        vinos_similares_list = encontrar_vino_similare_con_m_embeding.predecir_vinos_similares_con_m_embeding(sku)
+        vinos_con_precio_mas_similar =  filtar.filtrar_por_precio(sku, vinos_similares_list, df)
+        vinos_con_plan_marketin = plan_marketin.filtrar_vinos_con_plan_marketing(vinos_con_precio_mas_similar, df_bodega, df)
+        # unifico los vinos con plan de marketing y precio similares en un solo diccionario
+        # para luego pasárselo a la función que va a puntuar con su nivel de relevancia
+        vinos_precio_y_marketing = {'precio': vinos_con_precio_mas_similar, 'marketin': vinos_con_plan_marketin}
+        # aplico el nivel de relevancia 
+        vinos_con_relevancia = puntuadorDeVinos.asignar_puntos_de_relevancia(vinos_precio_y_marketing, vinos_similares_list)  
+        # elimino la descripción de los vinos
+        for vino in vinos_con_relevancia:
+            if 'description' in vino:
+                del vino['description']
+        return  vinos_con_relevancia
+    except KeyError:
+        raise HTTPException(status_code=404, detail="SKU no encontrado")
+    except ValueError as ve:
+        raise HTTPException(status_code=400, detail=str(ve))
     
-    # unifico los vinos con plan de marketing y precio similares en un solo diccionario
-    # para luego pasárselo a la función que va a puntuar con su nivel de relevancia
-    vinos_precio_y_marketing = {'precio': vinos_con_precio_mas_similar, 'marketin': vinos_con_plan_marketin}
-    
-    # aplico el nivel de relevancia 
-    vinos_con_relevancia = puntuadorDeVinos.asignar_puntos_de_relevancia(vinos_precio_y_marketing, vinos_similares_list)
-    
-    # elimino la descripción de los vinos
-    for vino in vinos_con_relevancia:
-        if 'description' in vino:
-            del vino['description']
-    
-    return  vinos_con_relevancia
